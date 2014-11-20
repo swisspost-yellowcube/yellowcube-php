6885e49664449c801da7dce3a90fc5112dba4077
Adrian Philipp <mail@adrian-philipp.com>
2014-11-20 16:10:28 +0100

    Removes generated code.

--- bin/generate.php
+++ bin/generate.php
@@ -10,8 +10,17 @@ $generator->generate(new Config(array(
     'inputFile'                      => __DIR__ . '/../YellowCubeService_009/YellowCubeService_extern.wsdl',
     'outputDir'                      => 'gen/YellowCube',
     'verbose'                        => false,
+    'namespaceName'                  => 'Service',
+    'sharedTypes'                    => false,
+    'constructorParamsDefaultToNull' => false
     'namespaceName'                  => 'YellowCube',
     'sharedTypes'                    => true,
     'constructorParamsDefaultToNull' => true,
     'soapClientClass'                => '\\YellowCube\Util\SoapClient'
+    'namespaceName'                  => 'Service',
+    'sharedTypes'                    => false,
+    'constructorParamsDefaultToNull' => false,
+    'soapClientOptions'              => array(
+        'features' => SOAP_USE_XSI_ARRAY_TYPE + SOAP_SINGLE_ELEMENT_ARRAYS
+    )
 )));
