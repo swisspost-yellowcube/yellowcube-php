2290a0f873772b703f772b5cd18f11e34ba8bb6b
Adrian Philipp <mail@adrian-philipp.com>
2014-11-24 15:34:29 +0100

    Removes accidentally added setters in minimal example.

--- test/add-article-minimal.php
+++ test/add-article-minimal.php
@@ -13,11 +13,7 @@ $article
     ->setDepositorNo('0000040750')
     ->setArticleNo('47686-3009-35/38')
     ->setNetWeight(0.042, ISO::KGM)
-    ->setEAN('7501031311309')
     ->setAlternateUnitISO(ISO::PCE)
-    ->setLength(120, ISO::MMT)
-    ->setWidth(80, ISO::MMT)
-    ->setHeight(20, ISO::MMT)
     ->addArticleDescription('Ich wars nicht.', 'de')
 ;
 
