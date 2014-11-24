ed822a27a52d4c59653140caa4be096f106fb1ac
Adrian Philipp <mail@adrian-philipp.com>
2014-11-24 09:48:16 +0100

    Use all available Article properties in example.

--- test/add-product.php
+++ test/add-product.php
@@ -5,10 +5,17 @@ use YellowCube\Art\Article;
 use YellowCube\Art\ChangeFlag;
 use YellowCube\Art\NetWeight;
 use YellowCube\Art\ArticleDescription;
+use YellowCube\Art\UnitsOfMeasure\AltDenominatorUOM;
 use YellowCube\Art\UnitsOfMeasure\AlternateUnitISO;
+use YellowCube\Art\UnitsOfMeasure\AltNumeratorUOM;
 use YellowCube\Art\UnitsOfMeasure\EAN;
 use YellowCube\Art\UnitsOfMeasure\EANType;
+use YellowCube\Art\UnitsOfMeasure\GrossWeight;
+use YellowCube\Art\UnitsOfMeasure\Height;
 use YellowCube\Art\UnitsOfMeasure\ISO;
+use YellowCube\Art\UnitsOfMeasure\Length;
+use YellowCube\Art\UnitsOfMeasure\Volume;
+use YellowCube\Art\UnitsOfMeasure\Width;
 use YellowCube\Config;
 
 $article = (new Article)
@@ -20,10 +27,20 @@ $article = (new Article)
     ->setNetWeight(new NetWeight(0.042, ISO::KGM))
     ->addUnitOfMeasure(new EAN('7501031311309', EANType::HE))
     ->addUnitOfMeasure(new AlternateUnitISO('PCE'))
+    ->addUnitOfMeasure(new AltNumeratorUOM('12'))
+    ->addUnitOfMeasure(new AltDenominatorUOM('12'))
+    ->addUnitOfMeasure(new GrossWeight(0.5, ISO::KGM))
+    ->addUnitOfMeasure(new Length(120, ISO::MMT))
+    ->addUnitOfMeasure(new Width(80, ISO::MMT))
+    ->addUnitOfMeasure(new Height(20, ISO::MMT))
+    ->addUnitOfMeasure(new Volume(20, ISO::CMQ))
     ->addArticleDescription(new ArticleDescription('Ich wars nicht.', 'de'))
     ->addArticleDescription(new ArticleDescription('Je te jure! Je n’ai rien fait!', 'fr'))
     ->addArticleDescription(new ArticleDescription('Non sono stato io!', 'it'))
-    ->addArticleDescription(new ArticleDescription('It wasn\'t me.', 'en'));
+    ->addArticleDescription(new ArticleDescription('It wasn\'t me.', 'en'))
+    ->setPeriodExpDateType(1)
+    ->setSerialNoFlag(true)
+;
 
 $client = new YellowCube\Client(Config::testConfig());
 $response = $client->insertArticleMasterData($article);
