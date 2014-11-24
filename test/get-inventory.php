0936edc0658df9e263cff1c85e65ca64a51b10f9
Adrian Philipp <mail@adrian-philipp.com>
2014-11-24 14:32:28 +0100

    Fixes typo.

--- test/get-inventory.php
+++ test/get-inventory.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use YellowCube\Config;
 
 $client = new YellowCube\Client(Config::testConfig());
-$articleList = $client->GetInventory();
+$articleList = $client->getInventory();
 
 var_dump($articleList);
 assert(count($articleList), 'Getting inventory failed.');
