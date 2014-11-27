b5b587d83404ce1d09d8c8cbc2c8607b65fe8655
Adrian Philipp <mail@adrian-philipp.com>
2014-11-27 16:27:43 +0100

    Adds all AdditionalServices to the example order.

--- test/add-order.php
+++ test/add-order.php
@@ -3,7 +3,20 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use YellowCube\WAB\AdditionalService\AdditionalShippingServices;
 use YellowCube\WAB\AdditionalService\BasicShippingServices;
+use YellowCube\WAB\AdditionalService\CODAccountNo;
+use YellowCube\WAB\AdditionalService\CODAmount;
+use YellowCube\WAB\AdditionalService\CODRefNo;
+use YellowCube\WAB\AdditionalService\DeliveryDate;
 use YellowCube\WAB\AdditionalService\DeliveryInstructions;
+use YellowCube\WAB\AdditionalService\DeliveryLocation;
+use YellowCube\WAB\AdditionalService\DeliveryPeriodeCode;
+use YellowCube\WAB\AdditionalService\DeliveryTimeFrom;
+use YellowCube\WAB\AdditionalService\DeliveryTimeJIT;
+use YellowCube\WAB\AdditionalService\DeliveryTimeTo;
+use YellowCube\WAB\AdditionalService\FloorNo;
+use YellowCube\WAB\AdditionalService\FrightShippingFlag;
+use YellowCube\WAB\AdditionalService\NotificationServiceCode;
+use YellowCube\WAB\AdditionalService\NotificationType;
 use YellowCube\WAB\Order;
 use YellowCube\WAB\OrderHeader;
 use YellowCube\WAB\Partner;
@@ -31,15 +44,25 @@ $partner
     ->setEmail('name@test.ch')
     ->setLanguageCode('en');
 
-$position = new Position();
-$position
+$position1 = new Position();
+$position1
     ->setPosNo(10)
-    ->setArticleNo('200522004')
+    ->setArticleNo('20052200')
     ->setEAN('714718003580')
     ->setPlant('Y010')
     ->setQuantity(1.00)
     ->setQuantityISO('PCE')
-    ->setShortDescription('loral Pasty Daisie');
+    ->setShortDescription('First Pasty Daisie');
+
+$position2 = new Position();
+$position2
+    ->setPosNo(10)
+    ->setArticleNo('20052004')
+    ->setEAN('714718003580')
+    ->setPlant('Y010')
+    ->setQuantity(1.00)
+    ->setQuantityISO('PCE')
+    ->setShortDescription('Another Pasty Daisie');
 
 $order = new Order();
 $order
@@ -47,8 +70,22 @@ $order
     ->setPartnerAddress($partner)
     ->addValueAddedService(new AdditionalShippingServices())
     ->addValueAddedService(new BasicShippingServices(BasicShippingServices::PRI))
-    ->addValueAddedService(new DeliveryInstructions(''))
-    ->addOrderPosition($position)
+    ->addValueAddedService(new DeliveryInstructions('Ring three times.'))
+    ->addValueAddedService(new DeliveryLocation('Last house in the street.'))
+    ->addValueAddedService(new DeliveryPeriodeCode(DeliveryPeriodeCode::MORNING))
+    ->addValueAddedService(new DeliveryTimeFrom('13:00'))
+    ->addValueAddedService(new DeliveryTimeTo('15:00'))
+    ->addValueAddedService(new DeliveryTimeJIT('14:00'))
+    ->addValueAddedService(new DeliveryDate(date('Ymd', strtotime('+4 days'))))
+    ->addValueAddedService(new CODAmount('150.00')) // COD amount in CHF (Cash on 153.45 Delivery).
+    ->addValueAddedService(new CODAccountNo('010683242'))
+    ->addValueAddedService(new CODRefNo('897201301070000097673'))
+    ->addValueAddedService(new FrightShippingFlag('1')) // Yes, fright consignment (small consignments).
+    ->addValueAddedService(new FloorNo('3'))
+    ->addValueAddedService(new NotificationType(NotificationType::EMAIL))
+    ->addValueAddedService(new NotificationServiceCode('2'))
+    ->addOrderPosition($position1)
+    ->addOrderPosition($position2)
     ->setOrderDocumentsFlag(true);
 
 $response = $service->createYCCustomerOrder($order);
