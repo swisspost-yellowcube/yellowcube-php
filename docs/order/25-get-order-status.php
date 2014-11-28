1fba1178002cf9c0e9bce86e1bc4e9b28993ef4f
Adrian Philipp <mail@adrian-philipp.com>
2014-11-28 10:32:06 +0100

    Changes order status Reference to another which doesn't return an error.

--- docs/order/25-get-order-status.php
+++ docs/order/25-get-order-status.php
@@ -3,7 +3,7 @@ require __DIR__ . '/../../../../../../vendor/autoload.php';
 
 ### "order-status"
 $service = new YellowCube\Service(YellowCube\Config::testConfig());
-$response = $service->getYCCustomerOrderStatus(1054);
+$response = $service->getYCCustomerOrderStatus(1183);
 
 ### "order-status-response"
 echo "Retrieved status for order with reference {$response->getReference()}." . PHP_EOL . PHP_EOL;
