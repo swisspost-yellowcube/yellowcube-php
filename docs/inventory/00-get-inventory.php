35ba4f0715c436441ec792cd6f42853373082b59
Adrian Philipp <mail@adrian-philipp.com>
2014-12-08 17:55:53 +0100

    Adds example getter-calls to the inventory (BAR) example.

--- docs/inventory/00-get-inventory.php
+++ docs/inventory/00-get-inventory.php
@@ -7,6 +7,26 @@ $articleList = $service->getInventory();
 
 echo "Retrieved " . count($articleList) . " articles from inventory." . PHP_EOL . PHP_EOL;
 
+foreach ($articleList as $article) {
+    echo "ArticleNo: "
+        .  $article->getArticleNo() . PHP_EOL;
+    echo "Quantity: " .  (string) $article->getQuantityUOM() . ' '
+        . $article->getQuantityUOM()->getQuantityISO() . PHP_EOL;
+    echo "ArticleDescription: "
+        .  $article->getArticleDescription() . PHP_EOL;
+    echo "Plant: "
+        .  $article->getPlant() . PHP_EOL;
+    echo "StorageLocation: "
+        .  $article->getStorageLocation() . PHP_EOL;
+    echo "YCLot: "
+        .  $article->getYCLot() . PHP_EOL;
+    echo "YCArticleNo: "
+        .  $article->getYCArticleNo() . PHP_EOL;
+
+    echo PHP_EOL . PHP_EOL;
+}
+
+echo "List of all data: ";
 print_r($articleList);
 
 
