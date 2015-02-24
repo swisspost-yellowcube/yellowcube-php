14a7eac6770f4954c933f684ed946ecc1fc7eb59
Adrian Philipp <mail@adrian-philipp.com>
2015-02-24 17:58:50 +0100

    Integrates WSASoap to sign SOAP messages using the certificate.

--- src/YellowCube/Util/SoapClient.php
+++ src/YellowCube/Util/SoapClient.php
@@ -2,6 +2,10 @@
 
 namespace YellowCube\Util;
 
+use Wse\WSASoap;
+use Wse\WSSESoap;
+use Wse\XMLSecurityKey;
+
 /**
  * Sets default options for SoapClient.
  *
@@ -11,6 +15,20 @@ namespace YellowCube\Util;
  */
 class SoapClient extends \SoapClient
 {
+    /**
+     * @var array Soap client options.
+     */
+    protected $options = array();
+
+    /**
+     * @var string Loaded certificate.
+     */
+    protected $certificateContent;
+
+    /**
+     * @param mixed $wsdl
+     * @param array $options
+     */
     public function __construct($wsdl, $options)
     {
         $defaultOptions = array(
@@ -35,7 +53,32 @@ class SoapClient extends \SoapClient
             )
         );
 
-        parent::__construct($wsdl, array_merge($defaultOptions, $options));
+        $this->options = array_merge($defaultOptions, $options);
+        
+        parent::__construct($wsdl, $this->options);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function __doRequest($request, $location, $action, $version, $one_way = NULL)
+    {
+        $doc = new \DOMDocument();
+        $doc->loadXML($request);
+
+        $wsse = new WSSESoap($doc);
+        $wsse->addTimestamp();
+
+        $key = new XMLSecurityKey(XMLSecurityKey::RSA_SHA1, array('type'=>'private'));
+        $key->loadKey($this->getCertificateContent());
+        $wsse->signSoapDoc($key);
+
+        $token = $wsse->addBinaryToken($this->getCertificateContent());
+        $wsse->attachTokentoSig($token);
+
+        $signedRequest = $wsse->saveXML();
+
+        return parent::__doRequest($signedRequest, $location, $action, $version);
     }
 
     /**
@@ -45,7 +88,7 @@ class SoapClient extends \SoapClient
      * @param string $args
      * @return mixed|void
      *
-     * @throws \Exception
+     * @throws YellowCubeException if a SOAP error occurs.
      */
     public function __call($method, $args)
     {
@@ -56,5 +99,18 @@ class SoapClient extends \SoapClient
             $message .= PHP_EOL . PHP_EOL . 'Request XML: ' . PHP_EOL . $this->__getLastRequest();
             throw new YellowCubeException($message, $e->getCode(), $e);
         }
+    }
+
+    /**
+     * Returns content of the certificate passed  in 'local_cert'.
+     *
+     * @return string Content of the certificate passed  in 'local_cert'.
+     */
+    protected function getCertificateContent() {
+        if (empty($this->certificateContent)) {
+            $this->certificateContent = file_get_contents($this->options['local_cert']);
+        }
+
+        return $this->certificateContent;
     }
 }
