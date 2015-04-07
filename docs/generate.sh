8ab06bb256c5150dd2f22c57d4522d15df6683e2
Adrian Philipp <mail@adrian-philipp.com>
2015-04-07 11:32:21 +0200

    Adds script to re-generate docs and push them to github.

---
+++ docs/generate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Install dependencies.
+pip install .
+
+# Run documentation generator.
+/bin/bash dexy
+
+# Change to the gh-pages branch.
+git config --global user.email "adrian.philipp@liip.ch"
+git config --global user.name "Travis build bot."
+git remote set-url origin "https://${GH_TOKEN}@github.com/swisspost-yellowcube/yellowcube-php.git"
+git fetch origin +gh-pages:gh-pages
+git checkout gh-pages
+
+# Update generated docs.
+cp -r output-site/* ../
+
+# Push docs to the github page.
+git add *
+git commit -m "Updates docs."
+git push origin gh-pages
+git checkout master
