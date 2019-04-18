e8f159af77d7a62c78f6f88a379d794505ff2bab
Sascha Grossenbacher <saschagros@gmail.com>
2019-04-18 11:31:11 +0200

    Add setter methods to BAR\Article to allow unit testing

--- src/YellowCube/BAR/Article.php
+++ src/YellowCube/BAR/Article.php
@@ -148,4 +148,126 @@ class Article
     {
         return $this->ProductionDate;
     }
+
+    /**
+     * @param $YCArticleNo
+     * @return \YellowCube\BAR\Article
+     */
+    public function setYCArticleNo($YCArticleNo)
+    {
+        $this->YCArticleNo = $YCArticleNo;
+        return $this;
+    }
+
+    /**
+     * @param $EAN
+     * @return \YellowCube\BAR\Article
+     */
+    public function setEAN($EAN)
+    {
+        $this->EAN = $EAN;
+        return $this;
+    }
+
+    /**
+     * @param $ArticleNo
+     * @return \YellowCube\BAR\Article
+     */
+    public function setArticleNo($ArticleNo)
+    {
+        $this->ArticleNo = $ArticleNo;
+        return $this;
+    }
+
+    /**
+     * @param $ArticleDescription
+     * @return \YellowCube\BAR\Article
+     */
+    public function setArticleDescription($ArticleDescription)
+    {
+        $this->ArticleDescription = $ArticleDescription;
+        return $this;
+    }
+
+    /**
+     * @param $Plant
+     * @return \YellowCube\BAR\Article
+     */
+    public function setPlant($Plant)
+    {
+        $this->Plant = $Plant;
+        return $this;
+    }
+
+    /**
+     * @param $StorageLocation
+     * @return \YellowCube\BAR\Article
+     */
+    public function setStorageLocation($StorageLocation)
+    {
+        $this->StorageLocation = $StorageLocation;
+        return $this;
+    }
+
+    /**
+     * @param $YCLot
+     * @return \YellowCube\BAR\Article
+     */
+    public function setYCLot($YCLot)
+    {
+        $this->YCLot = $YCLot;
+        return $this;
+    }
+
+    /**
+     * @param $Lot
+     * @return \YellowCube\BAR\Article
+     */
+    public function setLot($Lot)
+    {
+        $this->Lot = $Lot;
+        return $this;
+    }
+
+    /**
+     * @param $BestBeforeDate
+     * @return \YellowCube\BAR\Article
+     */
+    public function setBestBeforeDate($BestBeforeDate)
+    {
+        $this->BestBeforeDate = $BestBeforeDate;
+        return $this;
+    }
+
+    /**
+     * @param $ProductionDate
+     * @return \YellowCube\BAR\Article
+     */
+    public function setProductionDate($ProductionDate)
+    {
+        $this->ProductionDate = $ProductionDate;
+        return $this;
+    }
+
+    /**
+     * @param $StockType
+     * @return \YellowCube\BAR\Article
+     */
+    public function setStockType($StockType)
+    {
+        $this->StockType = $StockType;
+        return $this;
+    }
+
+    /**
+     * @param QuantityUOM $QuantityUOM
+     * @return \YellowCube\BAR\Article
+     */
+    public function setQuantityUOM(QuantityUOM $QuantityUOM)
+    {
+        $this->QuantityUOM = $QuantityUOM;
+        return $this;
+    }
+
+    
 }
